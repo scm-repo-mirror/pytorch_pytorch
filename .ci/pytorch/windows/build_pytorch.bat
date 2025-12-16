77df005405b45919433ae0838d69fe3e32b1db10
Han, Xu <xu.han@intel.com>
2025-12-16 16:02:21 +0800

    update code.

--- .ci/pytorch/windows/build_pytorch.bat
+++ .ci/pytorch/windows/build_pytorch.bat
@@ -69,8 +69,8 @@ if not "%CUDA_VERSION%" == "cpu" if not "%CUDA_VERSION%" == "xpu" (
 
 :: Install sccache
 if "%USE_SCCACHE%" == "1" (
+    mkdir %CD%\tmp_bin
     if not "%CUDA_VERSION%" == "xpu"(
-        mkdir %CD%\tmp_bin
         curl -k https://s3.amazonaws.com/ossci-windows/sccache.exe --output %CD%\tmp_bin\sccache.exe
         curl -k https://s3.amazonaws.com/ossci-windows/sccache-cl.exe --output %CD%\tmp_bin\sccache-cl.exe
     }
